995eba070a3e73e194a882360e5b2711dbd3aae3
Heytordesouza <heytordesnascimento@gmail.com>
2023-01-11 21:46:04 -0300

    Fluxo de Dados

--- src/index.ts
+++ src/index.ts
@@ -20,43 +20,156 @@ app.get("/accounts", (req: Request, res: Response) => {
     res.send(accounts)
 })
 
+
+///
+
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    
+    try {
+        const id = req.params.id
+        const result = accounts.find((account) => account.id === id) 
 
-    const result = accounts.find((account) => account.id === id) 
+         if(!result) {
+            res.status(404)
+            throw new Error ("Conta não encontrada. Verifique a 'id'.")
+         }
 
-    res.status(200).send(result)
+     res.status(200).send(result)
+    } catch (error:any) {
+        console.log(error)
+
+        if(res.statusCode === 200){
+            res.status(500)
+        }
+
+        res.send(error.message)
+    }  
 })
+
+
+// Validação de input:
+// Caso a id recebida não inicie com a letra ‘a’ será retornado um erro 400
+// Mensagem de erro:
+// 	“‘id’ inválido. Deve iniciar com letra ‘a’”
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+    try {
+        const id = req.params.id
+           
+           if(id[0] !== "a"){
+               res.status(400)
+               throw new Error ("'id' inválido. Deve ser iniciado com a letra 'a'.")
+           }
 
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
-    }
+        const accountIndex = accounts.findIndex((account) => account.id === id)
+    
+        if (accountIndex >= 0) {
+            accounts.splice(accountIndex, 1)
+            res.status(200).send("Item deletado com sucesso")
+        } else {
+            res.status(404).send("Item não encontrado")
+        }
 
-    res.status(200).send("Item deletado com sucesso")
+    } catch (error:any) {
+        console.log(error)
+
+        if(res.statusCode === 200){
+            res.status(500)
+        }
+        res.send(error.message)
+    }  
 })
 
+
+
+
+// req.body.balance (newBalance)
+// Deve ser number
+// Deve ser maior ou igual a zero
+
+// req.body.type (newType)
+// deve valer um dos tipos de conta do enum
+
+// Para fixar, agora é sua vez de implementar as seguintes validações:
+// req.body.id (newId)
+// string que inicia com a letra ‘a’
+
+// req.body.ownerName (newOwnerName)
+// string com no mínimo 2 caracteres
+
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+    try {
+        const id = req.params.id
 
-    const account = accounts.find((account) => account.id === id) 
+        const newId = req.body.id 
+        const newOwnerName = req.body.ownerName 
+        const newBalance = req.body.balance 
+        const newType = req.body.type 
 
-    if (account) {
-        account.id = newId || account.id
-        account.ownerName = newOwnerName || account.ownerName
-        account.type = newType || account.type
+        if(newBalance !== undefined){
+             
+            if(typeof newBalance !== "number"){
+                res.status(400)
+                throw new Error("'Balance' dever ser um número");
+            }
 
-        account.balance = isNaN(newBalance) ? account.balance : newBalance
-    }
+            if(newBalance<0){
+                res.status(400)
+                throw new Error("'Balance' dever ser maior que zero");
+            }
+        }
 
-    res.status(200).send("Atualização realizada com sucesso")
+        if(newType !== undefined){
+
+            if(
+                newType !== ACCOUNT_TYPE.GOLD &&
+                newType !== ACCOUNT_TYPE.PLATINUM &&
+                newType !== ACCOUNT_TYPE.BLACK ){
+                    res.status(400)
+                    throw new Error("'type' deve ser igual a Gold, platinum ou black")
+                }
+        }
+
+
+        if(newBalance !== undefined){
+
+            if(newId[0] !== "a"){
+                res.status(400)
+                throw new Error ("'id' inválido. Deve ser iniciado com a letra 'a'.")
+            }
+        } 
+
+
+        if(newOwnerName !== undefined){
+
+            if(newOwnerName.length < 2){
+                res.status(400)
+                throw new Error ("'ownerName' inválido. Deve ter no mínimo 2 caracteres.")
+            }
+        }
+
+
+        const account = accounts.find((account) => account.id === id) 
+
+        if (account) {
+            account.id = newId || account.id
+            account.ownerName = newOwnerName || account.ownerName
+            account.type = newType || account.type
+
+            account.balance = isNaN(newBalance) ? account.balance : newBalance
+        }
+
+        res.status(200).send("Atualização realizada com sucesso")
+
+    } catch (error:any) {
+        console.log(error)
+
+        if(res.statusCode === 200){
+            res.status(500)
+        }
+        res.send(error.message)
+    }  
+    
 })
